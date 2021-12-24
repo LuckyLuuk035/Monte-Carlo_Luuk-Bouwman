4570e2dc7d01ca865d85a4c3825e63d55af1ad3b
LuckyLuuk035 <luckyluuk035@icloud.com>
2021-12-24 17:25:38 +0100

    speelWedstrijd en TODO toegevoegd

--- Wedstrijd.py
+++ Wedstrijd.py
@@ -5,9 +5,20 @@ class Wedstrijd:
     def __init__(self, thuis, uit, kansen):
         self.thuis = thuis
         self.uit = uit
-        self.win = kansen[0]
-        self.gelijk = kansen[1]
-        self.verlies = kansen[2]
+        self.win = kansen[0] / 100
+        self.gelijk = (kansen[0] + kansen[1]) / 100
 
     def speelWedstrijd(self, seed):
-        print(self.thuis, self.uit, seed)
+        print(self.thuis.naam, self.uit.naam, self.win, self.gelijk, seed)
+        if self.win >= seed:
+            print(self.thuis.naam, "+3 punten")
+            print(self.uit.naam, "0 punten")
+        elif self.gelijk >= seed:
+            print(self.thuis.naam, self.uit.naam, "beide +1 punten")
+
+        elif seed < 1:
+            print(self.thuis.naam, "0 punten")
+            print(self.uit.naam, "+3 punten")
+        else:
+            raise IndexError
+        print("")
