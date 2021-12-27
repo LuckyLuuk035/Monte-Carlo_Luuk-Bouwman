cbf13d6368cee7af41cbcd20e413ba135c0e06dc
LuckyLuuk035 <luckyluuk035@icloud.com>
2021-12-27 14:53:10 +0100

    Update Voetbal_club.py
    comments toegevoegd

--- Voetbal_club.py
+++ Voetbal_club.py
@@ -1,4 +1,8 @@
 class VoetbalClub:
+    """
+    VoetbalClub is een simpele class.
+    Gemaakt om het aantal punten van de clubs bij te kunnen houden.
+    """
     def __init__(self, naam):
         self.naam = naam
         self.punten = 0
