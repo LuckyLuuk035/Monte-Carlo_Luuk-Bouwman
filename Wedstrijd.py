00342cb4b1abd00bfefb457f80c10f1117edf3c0
LuckyLuuk035 <luckyluuk035@icloud.com>
2021-12-27 14:51:58 +0100

    Update Wedstrijd.py
    comments toegevoegd

--- Wedstrijd.py
+++ Wedstrijd.py
@@ -1,27 +1,31 @@
 class Wedstrijd:
+    """
+    Wedstrijd is een simpele class.
+    """
     def __init__(self, thuis, uit, kansen):
+        """
+        :param thuis: het thuis spelende team.
+        :param uit: het uit spelende team.
+        :param kansen: een lijst van kansen op winnen, gelijkspelen of verliezen.
+        """
         self.thuis = thuis
         self.uit = uit
         self.win = kansen[0] / 100
         self.gelijk = (kansen[0] + kansen[1]) / 100
 
     def speelWedstrijd(self, seed):
-        # print(self.thuis.naam, self.uit.naam, self.win, self.gelijk, seed)
+        """
+        speelWedstrijd "speelt" de wedstrijden en geeft de teams het aantal punten.
+        :param seed: de seed is een 0-1 getal en bepaald welke partij wint.
+        """
         if self.win >= seed:
             self.thuis.punten += 3
-            # print(self.thuis.naam, "+3 punten. Totaal:", self.thuis.punten)
-            # print(self.uit.naam, "0 punten. Totaal:", self.uit.punten)
 
         elif self.gelijk >= seed:
             self.thuis.punten += 1
             self.uit.punten += 1
-            # print(self.thuis.naam, "+1 punt. Totaal:", self.thuis.punten)
-            # print(self.uit.naam, "+1 punt. Totaal:", self.uit.punten)
-
 
         elif seed < 1:
             self.uit.punten += 3
-            # print(self.thuis.naam, "0 punten. Totaal:", self.thuis.punten)
-            # print(self.uit.naam, "+3 punten. Totaal:", self.uit.punten)
         else:
             raise IndexError
