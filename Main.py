2c60feeb38925826f865512f9b221962d60ceb4e
LuckyLuuk035 <luckyluuk035@icloud.com>
2021-12-27 14:48:18 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,9 +1,6 @@
-from Middle_square import MidSquareGen
-
 from Monte_Carlo_simulatie import MonteCarloSimulatie
 
 seed = 12345
 
 simulatie = MonteCarloSimulatie(seed)
-
 print(simulatie.runSimulatie(2000))
