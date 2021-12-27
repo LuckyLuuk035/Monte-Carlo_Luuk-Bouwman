778145fd32fb58e73f4b0237f47cc4ed52531d63
LuckyLuuk035 <luckyluuk035@icloud.com>
2021-12-27 14:11:53 +0100

    voorspelling in percentages

--- Monte_Carlo_simulatie.py
+++ Monte_Carlo_simulatie.py
@@ -7,13 +7,27 @@ from Competitie import Competitie
 class MonteCarloSimulatie:
     def __init__(self, seed):
         self.competitie = Competitie(seed)
-        self.competitie.printStanding = True
-        self.competitie_scoreboard = pd.DataFrame(np.zeros((5, 5)), columns=[team.naam for team in self.competitie.teams])
-        self.competitie_scoreboard.index += 1
+        self.competitie.printStanding = False
+
+        self.scoreboard = pd.DataFrame(np.zeros((5, 5)),
+                                       columns=[team.naam for team in self.competitie.teams])
+        self.scoreboard.index += 1
+
+        self.voorspelling = self.scoreboard.copy()
 
     def runSimulatie(self, aantal):
         for i in range(aantal):
             resultaat = self.competitie.speelCompetitie()
             for j in resultaat:
-                self.competitie_scoreboard[j][resultaat.get(j)] += 1
-                print(self.competitie_scoreboard)
+                self.scoreboard[j][resultaat.get(j)] += 1
+        print(self.scoreboard)
+
+        self.updateVoorspelling(aantal)
+
+    def updateVoorspelling(self, aantal):
+        for kolom in self.scoreboard:
+            for index, value in enumerate(self.scoreboard[kolom]):
+                # print(kolom, index + 1, value)
+                if value != 0:
+                    self.voorspelling[kolom][index + 1] = (value / aantal) * 100
+        print(self.voorspelling)
